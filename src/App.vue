e9bb2ffe4e8e63c6d44e4a704db8bc85e99b7de5
Fernando Lamas <lamas.fernando1419@gmail.com>
2019-12-18 19:49:22 -0300

    Adding mainly components and their routes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h1>Welcome!</h1>
+        <router-view></router-view>
     </div>
 </template>
 
---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>The Portfolio Component</h2>
+</template>
