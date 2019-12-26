987acbe138be8b11359ee41ca1080035cde944d9
Fernando Lamas <lamas.fernando1419@gmail.com>
2019-12-25 23:28:57 -0300

    Refactoring same code in order to use mapGetters

--- src/components/stocks/StockList.vue
+++ src/components/stocks/StockList.vue
@@ -1,20 +1,19 @@
 <template>
     <div>
-        <app-stock v-for="(stock, index) in callGetStocks" :key="index" :stockData="stock"></app-stock>
+        <app-stock v-for="(stock, index) in getStocks" :key="index" :stockData="stock"></app-stock>
     </div>
 </template>
 
 <script>
 import Stock from './Stock.vue'
+import { mapGetters } from 'vuex';
 
 export default {
     components: {
         appStock: Stock
     },
     computed: {
-        callGetStocks() {
-            return this.$store.getters.getStocks;
-        }
+        ...mapGetters(['getStocks'])
     }
 }
 </script>
