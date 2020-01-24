ba40069dd0eeb113a39d94126a847c3287e4c8b5
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-01-23 21:58:18 -0300

    Deployed to heroku 2020-01-23 10pm

---
+++ dist/precache-manifest.bd43f73a2405debd3eeee9b958e7616a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5efb60a7a8359a5f27b4",
+    "url": "/css/app.3117f990.css"
+  },
+  {
+    "revision": "994f24881aaf517a8865514f058c6a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5efb60a7a8359a5f27b4",
+    "url": "/js/app.b5ab466f.js"
+  },
+  {
+    "revision": "e86743fa909d4b69aa88",
+    "url": "/js/chunk-vendors.526261c2.js"
+  },
+  {
+    "revision": "c3f8192302bee21f2bfc8140d8847ffc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
